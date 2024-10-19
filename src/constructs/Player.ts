56da85d1110eb087a8fd19d12206aa6f482c2813
tarunaidoo <taruna.naidoo05@gmail.com>
2024-10-19 22:25:37 +0200

    Revert timer back to 100 seconds

--- src/constructs/Player.ts
+++ src/constructs/Player.ts
@@ -46,7 +46,7 @@ export class Player extends Construct {
     placePrompt!: number;
     crosshair!: any;
     timer!:any;
-    timeRemaining: number = 20; // 2 minutes in seconds
+    timeRemaining: number = 100; // 2 minutes in seconds
     decrementValue!: number;
     timerInterval!: any;
     list!:any;
