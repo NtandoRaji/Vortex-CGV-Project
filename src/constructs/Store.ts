a1521feaa7f9c2332384a9da0719732383c89e9f
Tania908 <2486457@students.wits.ac.za>
2024-10-24 15:31:57 +0200

    commit #1

--- src/constructs/Store.ts
+++ src/constructs/Store.ts
@@ -23,6 +23,7 @@ import { VegSection2 } from "./VegSection2";
 import { VegSection3 } from "./VegSection3";
 import { IceCreamSection } from "./IceCreamSection";
 import { SectionE } from "./SectionE";
+import { fridgeSection } from "./fridgeSection";
 
 
 
@@ -106,7 +107,13 @@ export class Store extends Construct {
         const sectionE = new SectionE(graphics, physics, interactions, userInterface);
         this.sections.push(sectionE);
         this.addConstruct(sectionE);
+
+        const fridge_section = new fridgeSection(graphics, physics, interactions, userInterface);
+        this.sections.push(fridge_section);
+        this.addConstruct(fridge_section);
+    
     }
+    
 
     create(): void {    
         // Place Player
@@ -118,7 +125,7 @@ export class Store extends Construct {
 
         // --- Place Sections ---
         // TODO: Position new sections
-        const sectionsPositions = [[0, 0, -30], [30, 0, -27 , -30],[-25, 0, 30], [0, 0 ,  30], [30, 0, 27 , -30],[-78, 0, 0 , -60],[-25, 0 ,  -30],[60, 0, -27 , -30],[50, 0, -97, -30],[-10, 0, -97, -30],[-50, 0 ,  30]];
+        const sectionsPositions = [[0, 0, -30], [30, 0, -27 , -30],[-25, 0, 30], [0, 0 ,  30], [30, 0, 27 , -30],[-78, 0, 0 , -60],[-25, 0 ,  -30],[60, 0, -27 , -30],[50, 0, -97, -30],[-10, 0, -97, -30],[-50, 0 ,  30],[50, 0 ,-10]];
         for (let i = 0; i < this.sections.length; i++){
             const position = sectionsPositions[i];
             this.sections[i].root.position.set(position[0], position[1], position[2]);
---
+++ src/constructs/fridgeSection.ts
@@ -0,0 +1,80 @@
+// Import necessary modules and classes from 'three.js' and custom modules
+import * as THREE from "three";
+import { GraphicsContext, PhysicsContext } from "../lib";
+import { InteractManager } from "../lib/w3ads/InteractManager";
+import { InterfaceContext } from "../lib/w3ads/InterfaceContext";
+import { Shelf } from "./Shelf";
+import { PickupSpot } from "./PickupSpot";
+import { Section } from "./Section";
+
+// Define the SectionC class, which extends the Section class
+export class fridgeSection extends Section {
+    // Constructor method for SectionC to initialize necessary contexts
+    constructor(graphics: GraphicsContext, physics: PhysicsContext, interactions: InteractManager, userInterface: InterfaceContext) {
+        // Call the superclass constructor
+        super(graphics, physics, interactions, userInterface);
+    }
+
+    // Method to create shelves and boxes in the section and position them correctly
+    create(): void {
+        // --- Create and Position Shelves ---
+
+        // Array of shelf names representing the items
+        const shelfNames = ["fridgeTRY"];
+
+        // Predefined positions and rotations for each shelf
+        const shelfPositions = [
+            [0, 0, 17]
+        ];
+        const shelfRotations = [
+            [0, 0, 0]
+        ];
+
+        // Loop to create and position shelves with associated pickup spots
+        for (let i = 0; i < shelfNames.length; i++) {
+            console.log("creating", shelfNames[i]);
+            // Create a new PickupSpot object for each shelf
+            const pickupSpot = new PickupSpot(
+                this.graphics, 
+                this.physics, 
+                this.interactions, 
+                this.userInterface, 
+                [0, 0, 0], // Initial position
+                [3, 0.1, 8] // Scale (width, height, depth)
+            );
+
+            // Set the position and rotation for the pickup spot
+            pickupSpot.root.position.set(shelfPositions[i][0], 0, shelfPositions[i][2]);
+            pickupSpot.root.rotation.set(shelfRotations[i][0], shelfRotations[i][1], shelfRotations[i][2]);
+
+            // Add interaction logic for placing objects in the pickup spot
+            pickupSpot.interactions.addPickupSpot(pickupSpot.root, 10, (placeObject: THREE.Object3D) => {});
+
+            // Add the PickupSpot to the pickupSpots array and register it in the construct system
+            this.pickupSpots.push(pickupSpot);
+            this.addConstruct(pickupSpot);
+
+            // Create a new Shelf object for each item, with a specific name and scale
+            const shelf = new Shelf(
+                this.graphics, 
+                this.physics, 
+                this.interactions, 
+                this.userInterface, 
+                shelfNames[i], 
+                [1, 1, 1] // Scale (x, y, z)
+            );
+
+            // Set the position and rotation for each shelf
+            shelf.root.position.set(shelfPositions[i][0], shelfPositions[i][1], shelfPositions[i][2]);
+            shelf.root.rotation.set(shelfRotations[i][0], shelfRotations[i][1], shelfRotations[i][2]);
+
+            // Add interaction logic for picking up the shelf
+            shelf.interactions.addPickupObject(shelf.root, 8, 0.1, () => {});
+
+            // Add the shelf to the items array and register it as a construct
+            this.items.push(shelf);
+            this.addConstruct(shelf);
+        }
+        // -----------------------------
+    }
+}
